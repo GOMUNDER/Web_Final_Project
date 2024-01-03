23de6e33fb4de24d5f6508d14226fe6a9dbdd8de
GOMUNDER <willyhong712@gmail.com>
2024-01-03 22:44:41 +0800

    third commit

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     image = models.ImageField(null=True, blank=True)
-    #digital = models.BooleanField(default=False, null=True, blank=False)
+    #
 
     def __str__(self):
         return self.name
